e69332dde0f912f2783652fa97273eb3b0e1d91f
Amanjot <155871189+AmanjotKaurr@users.noreply.github.com>
2025-04-22 22:38:29 +0530

    Create EASY_22BCS16416 - Student.java

---
+++ EASY_22BCS16416 - Student.java
@@ -0,0 +1,15 @@
+// Student.java - Represents a student with a course
+public class Student {
+    private String name;
+    private Course course;
+
+    public Student(String name, Course course) {
+        this.name = name;
+        this.course = course;
+    }
+
+    public void displayInfo() {
+        System.out.println("Student: " + name);
+        System.out.println("Course: " + course);
+    }
+}
