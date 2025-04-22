37bd6486c428067d33a1f8bda3b5591af77b76f8
Amanjot <155871189+AmanjotKaurr@users.noreply.github.com>
2025-04-22 22:42:02 +0530

    Create MEDIUM_22BCS16416 - HibernateUtil.java

---
+++ MEDIUM_22BCS16416 - HibernateUtil.java
@@ -0,0 +1,19 @@
+// HibernateUtil.java - Provides SessionFactory using hibernate.cfg.xml
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
