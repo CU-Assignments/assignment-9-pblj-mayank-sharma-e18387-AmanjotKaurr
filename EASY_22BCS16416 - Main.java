3cbf09c314ebadf796a7a363bc14924e487f7ad0
Amanjot <155871189+AmanjotKaurr@users.noreply.github.com>
2025-04-22 22:40:21 +0530

    Create EASY_22BCS16416 - Main.java

---
+++ EASY_22BCS16416 - Main.java
@@ -0,0 +1,11 @@
+// Main.java - Loads Spring context and prints student details
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Student student = context.getBean(Student.class);
+        student.displayInfo();
+    }
+}
